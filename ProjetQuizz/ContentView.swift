401236fd2445aa813b708c70847f2825a40a777a
Darkcou <benjamingronie@gmail.com>
2020-03-16 16:00:13 +0100

    Initial Commit

---
+++ ProjetQuizz/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  ProjetQuizz
+//
+//  Created by Benjamin Gronier on 16/03/2020.
+//  Copyright © 2020 Benjamin Gronier. All rights reserved.
+//
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
